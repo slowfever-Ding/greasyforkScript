a0b0ba04a4c572aaf7caf3cf463b19c7f6ae6108
slowfever <slowfever.ding@gmail.com>
2025-04-10 17:23:34 +0800

    💕Initial commit

---
+++ util/markclickedjavlibrarylinksasvisited.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name        点击 JAVLibrary 链接标记为已访问
+// @author      slowFever
+// @namespace   https://greasyfork.org/
+// @description:zh   将点击过的 JAVLibrary 链接标记为已访问
+// @description:en   Mark clicked JAVLibrary links as visited
+// @match       *://*javlibrary.com/*
+// @match       *://*javlibrary*/*
+// @grant       GM_addStyle
+// @version     1.0.0
+// @description 2025/4/10 17:05:49
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    GM_addStyle(`
+        .pubgroup td a::before {
+            content: "●";
+            color: #1a9317;
+            padding-right: 4px;
+        }
+        
+        .pubgroup td a:visited::before {
+            color: #aaa;
+        }
+    `);
+
+})()
